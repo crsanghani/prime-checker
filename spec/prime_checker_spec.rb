890ebe9354da5c28655283fcf1be227e9af57c83
Chetan Sanghani <crsanghani@gmail.com>
2016-09-24 13:54:04 +0100

    rewrote the tests to fit what I'm trying to do and added a new one

--- spec/prime_checker_spec.rb
+++ spec/prime_checker_spec.rb
@@ -6,10 +6,14 @@ describe 'Prime'do
   end
 
   it 'Returns "This is a prime" when passed 2' do
-    expect(@Prime.checker(2)).to eq 'This is a prime'
+    expect(@Prime.checker(2)).to eq true
   end
 
   it 'Returns "This is not a prime" when passed 4' do
-    expect(@Prime.checker(4)).to eq 4
+    expect(@Prime.checker(4)).to eq false
+  end
+
+  it 'Returns "This is not a prime" when passed 6' do
+    expect(@Prime.checker(6)).to eq false
   end
 end
